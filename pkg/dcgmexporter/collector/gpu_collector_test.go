eaa8603bbd85cc5397ab6b2c6ea8a6fb54cfa153
Rohit Arora <roarora@nvidia.com>
2024-03-21 08:54:09 -0400

    intermediate

---
+++ pkg/dcgmexporter/collector/gpu_collector_test.go
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package collector
+
+import (
+	"testing"
+
+	"github.com/NVIDIA/go-dcgm/pkg/dcgm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	mock_dcgmprovider "github.com/NVIDIA/dcgm-exporter/mocks/pkg/dcgmexporter/dcgmprovider"
+	mock_sysinfo "github.com/NVIDIA/dcgm-exporter/mocks/pkg/dcgmexporter/sysinfo"
+	"github.com/NVIDIA/dcgm-exporter/pkg/common"
+	"github.com/NVIDIA/dcgm-exporter/pkg/dcgmexporter/dcgmprovider"
+	"github.com/NVIDIA/dcgm-exporter/pkg/dcgmexporter/sysinfo"
+)
+
+var sampleCounters = []common.Counter{
+	{1, "COUNTER_SAMPLE_1", "gauge", "Sample Counter 1"},
+	{2, "COUNTER_SAMPLE_2", "gauge", "Sample Counter 2"},
+}
+
+func Test_NewDCGMCollector(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDCGMProvider := mock_dcgmprovider.NewMockDCGMProvider(ctrl)
+	mockSysInfo := mock_sysinfo.NewMockSystemInfoInterface(ctrl)
+	mockSysInfo.EXPECT().InfoType().Return(dcgm.FE_GPU).MinTimes(0)
+	mockSysInfo.EXPECT().GOpts().Return(common.DeviceOptions{
+		Flex:       false,
+		MajorRange: []int{-1},
+		MinorRange: []int{-1},
+	})
+	mockSysInfo.EXPECT().GPUCount().Return(1).MinTimes(0)
+	mockSysInfo.EXPECT().GPU(0).Return(sysinfo.GPUInfo{
+		DeviceInfo:   dcgm.Device{GPU: 1},
+		GPUInstances: []sysinfo.GPUInstanceInfo{},
+		MigEnabled:   false,
+	}).MinTimes(0)
+
+	dcgmprovider.SetClient(mockDCGMProvider)
+
+	// Mock DCGM calls
+	mockGroupHandle := dcgm.GroupHandle{}
+	mockGroupHandle.SetHandle(uintptr(1))
+	mockDCGMProvider.EXPECT().CreateGroup(gomock.Any()).Return(mockGroupHandle, nil)
+	mockDCGMProvider.EXPECT().AddEntityToGroup(mockGroupHandle, gomock.Any(), gomock.Any()).Return(nil).MinTimes(0)
+	mockDCGMProvider.EXPECT().DestroyGroup(mockGroupHandle).MinTimes(0)
+
+	mockFieldHandle := dcgm.FieldHandle{}
+	mockFieldHandle.SetHandle(uintptr(1))
+	mockDCGMProvider.EXPECT().FieldGroupCreate(gomock.Any(), gomock.Any()).Return(mockFieldHandle, nil).MinTimes(0)
+	mockDCGMProvider.EXPECT().FieldGroupDestroy(mockFieldHandle).MinTimes(0).MinTimes(0)
+
+	mockDCGMProvider.EXPECT().WatchFieldsWithGroupEx(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(),
+		gomock.Any()).Return(nil).MinTimes(0)
+
+	config := &common.Config{}
+
+	// Set up the expected collector
+	expectedCollector := &DCGMCollector{
+		Counters: sampleCounters,
+		SysInfo:  mockSysInfo,
+		Hostname: "test-hostname",
+	}
+
+	fieldEntity := sysinfo.FieldEntityGroupTypeSystemInfoItem{
+		SystemInfo:   mockSysInfo,
+		DeviceFields: []dcgm.Short{1, 2, 3},
+	}
+
+	collector, cleanups, err := NewDCGMCollector(sampleCounters, "test-hostname", config, fieldEntity)
+
+	require.NoError(t, err)
+
+	assert.Equal(t, expectedCollector, collector)
+
+	assert.NotNil(t, cleanups)
+	cleanups()
+}
